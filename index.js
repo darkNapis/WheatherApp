86e318a54050749ca034754b7b7cd5e87f1ee170
darkNapis <nikolic.apis.darko@gmail.com>
2021-03-11 13:36:40 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const weatherData = {
+    tempUnit: 'C',
+    windSpeedUnit: 'm/s',
+    days: [
+    { day: 'Mon', temp: 22, windDirection: 'north-east', windSpeed: 23 , type:'sunny' },
+    { day: 'Tue', temp: 14, windDirection: 'north-west', windSpeed: 14, type: 'rainy' },
+    { day: 'Wed', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'cloudy' },
+    { day: 'Thu', temp: 22, windDirection: 'north-east', windSpeed: 25 , type:'sunny' },
+    { day: 'Fri', temp: 14, windDirection: 'north-west', windSpeed: 18, type: 'rainy' },
+    { day: 'Sat', temp: 17, windDirection: 'south-east', windSpeed: 17, type: 'cloudy' },
+    { day: 'Sun', temp: 17, windDirection: 'south-east', windSpeed: 16, type: 'cloudy' },
+    ]
+    };
+
+function dayClicked() {
+    const dayWrapper = document.createElement('div');
+    dayWrapper.append(this.day + " " + this.temp + " " + weatherData.tempUnit + " " + this.windDirection + " " + this.windSpeed + " " + this.type + " ");
+    const dayInfo = document.querySelector(".dayInfo");
+    dayInfo.innerHTML="";
+    dayInfo.appendChild(dayWrapper);
+} 
+
+function createList() {
+    const list = document.querySelector(".list");
+    for (let index = 0; index < weatherData.days.length; index++) {
+        const element = weatherData.days[index];
+        const dayWrapper = document.createElement("div");
+        dayWrapper.addEventListener("click", dayClicked.bind(element))
+        dayWrapper.append(element.day + " " + element.temp + " " + weatherData.tempUnit);
+        list.appendChild(dayWrapper);
+    }
+}    
+
+createList();
